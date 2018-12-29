22f629850e0f6dd3d9fd6c39b4013759ba860f07
dinosoup1 <45948999+dinosoup1@users.noreply.github.com>
2018-12-29 16:01:43 -0700

    Initial commit

---
+++ Dataset.py
@@ -0,0 +1,27 @@
+import requests
+import time
+
+start_date = 1514764800 * 1000 # January 1st 2018
+pair = "ETHBTC"
+timeframe = "1m"
+
+final_data=[]
+
+for _ in range(0,100):
+
+    url = "https://api.bitfinex.com/v2/candles/trade:"+timeframe+":t"+pair+"/hist?sort=1&limit=1000&start="+str(start_date)
+
+    r = requests.get(url)
+
+    temp_data = r.json()
+    final_data = final_data+temp_data
+    start_date = temp_data[len(temp_data)-1][0]+60
+
+    print(time.ctime()+" "+str(len(temp_data)))
+
+    time.sleep(2)
+
+print(final_data)
+
+with open("BFX_"+pair+"_"+timeframe+".json","w") as f:
+    f.write(str(final_data))
